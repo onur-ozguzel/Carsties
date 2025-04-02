dfc213293e781a3a4db7c32f2ffd193e1432b813
Onur Ozguzel <ozguzel.onur@gmail.com>
2025-04-02 10:54:16 +0200

    End of section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -5,29 +5,36 @@ namespace IdentityService;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
+        [
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-        };
+        ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-        {
+        [
             new ApiScope("auctionApp", "Auction app full access")
-        };
+        ];
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-        {
-            new Client
-            {
+        [
+            new() {
                 ClientId = "postman",
                 ClientName = "postman",
                 AllowedScopes = { "openid", "profile", "auctionApp" },
                 RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
-                ClientSecrets = new [] { new Secret("NotASecret".Sha256()) },
-                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }                
+                ClientSecrets = [ new Secret("NotASecret".Sha256()) ],
+                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new() {
+                ClientId = "nextapp",
+                ClientName = "nextapp",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                ClientSecrets = [ new Secret("SecondNotASecret".Sha256()) ],
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                AllowOfflineAccess = true,
+                AccessTokenLifetime = 3600 * 24 * 30
             }
-        };
+        ];
 }
